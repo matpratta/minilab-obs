2a481ffee146ea34d908d49f388bc2dd33da7f08
Matheus Pratta <matpratta@gmail.com>
2019-09-27 22:03:59 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,71 @@
+// Biblioteca easymidi
+const midi = require('easymidi')
+
+// Biblioteca OBSD
+const obsWS = require('obs-websocket-js')
+
+// Obter lista de inputs MIDI
+const midiInputs = midi.getInputs()
+
+// Selecionar nossa controladora
+let minilabInput = midiInputs.filter(input => ~input.indexOf('Arturia MiniLab mkII')).shift()
+
+let i = 0
+
+// A controladora foi encontrada?
+if (!minilabInput) {
+  // Não, então retornamos erro
+  console.error('Nenhuma controladora compatível encontrada.')
+} else {
+  // Sim, então conectamos à controladora
+  const minilab = new midi.Input(minilabInput)
+  const minilabOut = new midi.Output(minilabInput)
+
+  // Biblioteca de render
+  const render = require('./leds').init(minilabOut)
+  
+  // Lista de cenas
+  let cenas = []
+
+  // Conectamos ao OBS
+  const obs = new obsWS()
+  obs.connect({ address: '127.0.0.1:4444' })
+  .then(() => {
+    // Estamos conectados ao OBS aqui
+    // Obter lista de cenas
+    return obs.send('GetSceneList')
+  })
+  .then(data => {
+    // Criamos a lista de cenas
+    cenas = data.scenes.map(scene => scene.name)
+
+    minilab.on('noteon', msg => {
+      // Tipo de evento
+      let evento = {
+        tipo: 'geral'
+      }
+
+      // Categoriza tipo de evento para pads
+      if (msg.note >= 36 && msg.note <= 43) {
+        evento.tipo = 'pads'
+        evento.tecla = msg.note - 36
+      }
+
+      // Se for pad
+      if (evento.tipo == 'pads') {
+        let cenaNome = cenas[evento.tecla]
+        if (cenaNome) {
+          obs.send('SetCurrentScene', { 'scene-name': cenaNome })
+        }
+      }
+    })
+  })
+
+  // Quando mudar de cena
+  obs.on('SwitchScenes', data => {
+    let cenaId = cenas.indexOf(data.sceneName)
+
+    render.clear()
+    render.color(cenaId, 'magenta')
+  })
+}
